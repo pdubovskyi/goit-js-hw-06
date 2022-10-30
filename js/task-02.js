f6ecae95e904181a600cd0b4a13b8dd052ef6e01
pdubovskyi <petro.dubovskyi@gmail.com>
2022-10-30 19:45:50 +0200

    The task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+console.log(ingredientsListEl);
+
+const ingredientsItemsEl = ingredients => {
+  return ingredients.forEach(element => {
+    const newTag = document.createElement('li');
+    newTag.textContent = element;
+    newTag.classList.add('item');
+    ingredientsListEl.appendChild(newTag);
+  });
+};
+
+ingredientsItemsEl(['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments']);

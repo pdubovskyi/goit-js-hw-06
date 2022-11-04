ce0df3b5a3a6f76921af8b0c7d0b1d6c2b9ff558
pdubovskyi <petro.dubovskyi@gmail.com>
2022-11-04 21:15:29 +0200

    Task 4

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+const valueEl = document.querySelector('#value');
+valueEl.textContent = 0;
+
+const decrBtn = document.querySelector('button[data-action="decrement"]');
+const onDecrement = event => {
+  return (valueEl.textContent = -Math.abs(+decrBtn.textContent + Number(valueEl.textContent)));
+};
+decrBtn.addEventListener('click', onDecrement);
+
+const incrBtn = document.querySelector('button[data-action="increment"]');
+const onIncrement = event => {
+  return (valueEl.textContent = Number(valueEl.textContent) + 1);
+};
+incrBtn.addEventListener('click', onIncrement);
